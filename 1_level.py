c95565c7ed65e9c65bb7ee659dacbac6bbcc1dbe
Alexey Ershov <alex4rom@gmail.com>
2017-11-12 00:13:32 +0300

    Update 1_level.py

--- 1_level.py
+++ 1_level.py
@@ -1,3 +1,6 @@
 def flip_horizontal_axis(matrix):
     matrix[0], matrix[-1] = matrix[-1], matrix[0]
     return matrix
+
+def is_palindrome(input_string):
+    return input_string == input_string[::-1]
